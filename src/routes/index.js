86f299548ea9b52c2280dfa078225c35b125ead7
msyafiulmasruri <msyafiulmasruri@gmail.com>
2026-07-16 13:19:56 +0700

    chore: delete old folder

--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import authenticationRoutes from '../services/authentications/routes/index.js';
-
-const router = Router();
-
-router.use('/auth', authenticationRoutes);
-
-export default router;
--- src/services/users/validator/schema.js
+++
@@ -1,8 +0,0 @@
-import Joi from 'joi';
-
-export const updateMePayloadSchema = Joi.object({
-  name: Joi.string().trim().min(1).required().messages({
-    'any.required': 'Nama diperlukan.',
-    'string.empty': 'Nama tidak boleh kosong.',
-  }),
-});
